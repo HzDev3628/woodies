21b59661d39f00205e191eeaa7e5c864f2b3d17a
HzDev3628 <artem.khar5uk@gmail.com>
2023-10-31 14:19:22 +0200

    fix EsLint

--- src/app/[pages]/page.tsx
+++ src/app/[pages]/page.tsx
@@ -5,7 +5,7 @@ type GetPages = {
 export default async function Pages({ params: { pages } }: GetPages) {
   return (
     <main className="min-h-[calc(100vh-140px)]">
-      <div className="font-cherry flex w-full justify-center text-[96px] font-normal">
+      <div className="flex w-full justify-center font-cherry text-[96px] font-normal">
         {pages}
       </div>
     </main>
