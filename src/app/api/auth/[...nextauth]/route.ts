210811f3eb23b0aba452131c7a42b74f55b146ee
HzDev3628 <artem.khar5uk@gmail.com>
2023-11-05 16:47:02 +0200

    nextAuthUrl

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,6 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import { getCsrfToken } from 'next-auth/react'
 
 import { Signature } from '@/lib/Authentication/Signature'
-import { getURL } from '@/lib/hooks/getURL'
 
 const providers = [
   CredentialsProvider({
@@ -21,11 +20,9 @@ const providers = [
     async authorize(credentials, req) {
       const { publicKey, host } = JSON.parse(credentials?.message || '{}')
 
-      const nextAuthUrl = getURL()
-      console.log(nextAuthUrl)
-      console.log(nextAuthUrl)
+      const nextAuthUrl = new URL(process.env.NEXT_PUBLIC_VERCEL_URL || "");
 
-      if (host !== nextAuthUrl) {
+      if (host !== nextAuthUrl.host) {
         return null
       }
       const crsf = await getCsrfToken({ req: { ...req, body: null } })
