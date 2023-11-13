2cf090f0a912ece74fb977e18a29ab3e6bc8f5ba
HzDev3628 <artem.khar5uk@gmail.com>
2023-11-13 11:35:37 +0200

    Update organization auth route

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,68 @@
+import type { AuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { getCsrfToken } from 'next-auth/react'
+
+import { Signature } from '@/lib/Authentication/Signature'
+import { getURL } from '@/lib/hooks/getURL'
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'web3-auth',
+      credentials: {
+        signature: {
+          label: 'Signature',
+          type: 'text'
+        },
+        message: {
+          label: 'Message',
+          type: 'text'
+        }
+      },
+      async authorize(credentials, req) {
+        const { publicKey, host } = JSON.parse(credentials?.message || '{}')
+
+        console.log(getURL())
+        const nextAuthUrl = new URL(getURL() || '')
+
+        if (host !== nextAuthUrl.host) {
+          return null
+        }
+        const crsf = await getCsrfToken({ req: { ...req, body: null } })
+
+        if (!crsf) {
+          return null
+        }
+        const nonceUnit8 = Signature.create(crsf)
+
+        const isValidate = await Signature.validate(
+          {
+            signature: credentials?.signature || '',
+            publicKey
+          },
+          nonceUnit8
+        ).then((res) => res.valueOf)
+
+        if (!isValidate) {
+          throw new Error('Could not validate the signed message')
+        }
+
+        return { id: publicKey }
+      }
+    })
+  ],
+
+  session: { strategy: 'jwt' },
+
+  debug: process.env.NEXT_PUBLIC_VERCEL_ENV === 'development',
+
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.name = token.sub
+        session.user.image = `https://ui-avatars.com/api/?name=${token.sub}`
+      }
+      return session
+    }
+  }
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,69 +1,7 @@
-import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import { getCsrfToken } from 'next-auth/react'
+import NextAuth from 'next-auth/next'
 
-import { Signature } from '@/lib/Authentication/Signature'
-import { getURL } from '@/lib/hooks/getURL'
+import { authOptions } from './authOptions'
 
-const providers = [
-  CredentialsProvider({
-    name: 'web3-auth',
-    credentials: {
-      signature: {
-        label: 'Signature',
-        type: 'text'
-      },
-      message: {
-        label: 'Message',
-        type: 'text'
-      }
-    },
-    async authorize(credentials, req) {
-      const { publicKey, host } = JSON.parse(credentials?.message || '{}')
+const handler = NextAuth(authOptions)
 
-      console.log(getURL())
-      const nextAuthUrl = new URL(getURL() || '')
-
-      if (host !== nextAuthUrl.host) {
-        return null
-      }
-      const crsf = await getCsrfToken({ req: { ...req, body: null } })
-
-      if (!crsf) {
-        return null
-      }
-      const nonceUnit8 = Signature.create(crsf)
-
-      const isValidate = await Signature.validate(
-        {
-          signature: credentials?.signature || '',
-          publicKey
-        },
-        nonceUnit8
-      )
-
-      if (!isValidate) {
-        throw new Error('Could not validate the signed message')
-      }
-
-      return { id: publicKey }
-    }
-  })
-]
-
-const handler = NextAuth({
-  session: {
-    strategy: 'jwt'
-  },
-  providers,
-  callbacks: {
-    session({ session, token }) {
-      if (session.user) {
-        session.user.name = token.sub
-        session.user.image = `https://ui-avatars.com/api/?name=${token.sub}`
-      }
-      return session
-    }
-  }
-})
 export { handler as GET, handler as POST }
